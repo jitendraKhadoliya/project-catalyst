3c7cf23e05fe887b5fdf92110b5ad08dca08277c
jitendra <jitendrasaini352jjnu@gmail.com>
2023-06-30 00:10:11 +0530

    improved performence of the bubbles

--- app/page.js
+++ app/page.js
@@ -1,232 +1,12 @@
-// //homepage
-// "use client";
-// import React, { useContext } from "react";
-// import { StackDataContext } from "../utils/StackContext";
-// import data from "../constant/FinalData.json";
-// import "./homepage.css";
-
-// const Home = () => {
-//   const { selectedAddress } = useContext(StackDataContext);
-
-//   const selectedData = data.find(
-//     (item) => item.stake_address === selectedAddress
-//   );
-
-//   return (
-//     <div>
-//       <div>
-//         {/* <iframe
-//           className="h-[70vh] w-full"
-//           src="https://app.bubblemaps.io/bsc/token/0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95"
-//         /> */}
-//       </div>
-//       {selectedData ? (
-//         <div>
-//           <h2>Stack Address: {selectedData.stake_address}</h2>
-//           <p>Total Balance: {selectedData.total_balance}</p>
-//           <p>Total UTxO: {selectedData.total_utxo}</p>
-//           <h3>Outgoing Transactions:</h3>
-//           <ul>
-//             {selectedData.outgoing_transactions.map((transaction, idx) => (
-//               <li key={idx}>
-//                 <p>Address: {transaction.address}</p>
-//                 <p>Value: {transaction.value}</p>
-//               </li>
-//             ))}
-//           </ul>
-//           {/* Display other data here */}
-//         </div>
-//       ) : (
-//         <p>Please select a stake address from the navbar.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// "use client";
-// import React, { useContext, useEffect, useRef } from "react";
-// import { StackDataContext } from "../utils/StackContext";
-// import data from "../constant/FinalData.json";
-// import * as d3 from "d3";
-// import "./homepage.css";
-
-// const Home = () => {
-//   const { selectedAddress } = useContext(StackDataContext);
-//   const chartRef = useRef(null);
-
-//   useEffect(() => {
-//     // Find the selected data for the stack address
-//     const selectedData = data.find(
-//       (item) => item.stake_address === selectedAddress
-//     );
-
-//     // Clear any previous chart
-//     d3.select(chartRef.current).selectAll("*").remove();
-
-//     // Create a new chart if data is available
-//     if (selectedData) {
-//       const width = 600;
-//       const height = 400;
-
-//       // Define the bubble chart layout
-//       const bubble = d3.pack().size([width, height]).padding(1.5);
-
-//       // Create a hierarchy of data for the bubble chart
-//       const root = d3
-//         .hierarchy({ children: selectedData.outgoing_transactions })
-//         .sum((d) => d.value);
-
-//       // Generate the bubble positions and sizes
-//       bubble(root);
-
-//       // Create the SVG container for the chart
-//       const svg = d3
-//         .select(chartRef.current)
-//         .append("svg")
-//         .attr("width", width)
-//         .attr("height", height);
-
-//       // Create the bubble elements
-//       const bubbles = svg
-//         .selectAll(".bubble")
-//         .data(root.children)
-//         .enter()
-//         .append("g")
-//         .attr("class", "bubble")
-//         .attr("transform", (d) => `translate(${d.x},${d.y})`);
-
-//       // Add circles to represent the bubbles
-//       bubbles
-//         .append("circle")
-//         .attr("r", (d) => d.r)
-//         .style("fill", "steelblue");
-
-//       // Add text labels to the bubbles
-//       bubbles
-//         .append("text")
-//         .attr("dy", ".3em")
-//         .style("text-anchor", "middle")
-//         .text((d) => d.data.address);
-//     }
-//   }, [selectedAddress]);
-
-//   return (
-//     <div>
-//       <div ref={chartRef} className="chart"></div>
-//       {/* Rest of your code */}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// !
-// "use client";
-// import React, { useContext, useEffect, useRef, useState } from "react";
-// import { StackDataContext } from "../utils/StackContext";
-// import data from "../constant/FinalData.json";
-// import * as d3 from "d3";
-// import "./homepage.css";
-
-// const Home = () => {
-//   const { selectedAddress } = useContext(StackDataContext);
-//   const chartRef = useRef(null);
-//   const [selectedBubble, setSelectedBubble] = useState(null);
-
-//   useEffect(() => {
-//     // Find the selected data for the stack address
-//     const selectedData = data.find(
-//       (item) => item.stake_address === selectedAddress
-//     );
-//     const handleClick = (d) => {
-//       setSelectedBubble(d);
-//     };
-
-//     // Clear any previous chart
-//     d3.select(chartRef.current).selectAll("*").remove();
-
-//     // Create a new chart if data is available
-//     if (selectedData) {
-//       const width = 600;
-//       const height = 400;
-
-//       // Define the bubble chart layout
-//       const bubble = d3.pack().size([width, height]).padding(1.5);
-
-//       // Create a hierarchy of data for the bubble chart
-//       const root = d3
-//         .hierarchy({ children: selectedData.outgoing_transactions })
-//         .sum((d) => d.value);
-
-//       // Generate the bubble positions and sizes
-//       bubble(root);
-
-//       // Create the SVG container for the chart
-//       const svg = d3
-//         .select(chartRef.current)
-//         .append("svg")
-//         .attr("width", width)
-//         .attr("height", height);
-
-//       // Create the bubble elements
-//       const bubbles = svg
-//         .selectAll(".bubble")
-//         .data(root.children)
-//         .enter()
-//         .append("g")
-//         .attr("class", "bubble")
-//         .attr("transform", (d) => `translate(${d.x},${d.y})`)
-//         .on("click", (d) => setSelectedBubble(d));
-
-//       // Add circles to represent the bubbles
-//       bubbles
-//         .append("circle")
-//         .attr("r", (d) => d.r)
-//         .style("fill", "steelblue");
-
-//       // Add text labels to the bubbles
-//       bubbles
-//         .append("text")
-//         .attr("dy", ".3em")
-//         .style("text-anchor", "middle")
-//         .text((d) => d.data.address);
-//     }
-//   }, [selectedAddress]);
-
-//   const handleClosePopup = () => {
-//     setSelectedBubble(null);
-//   };
-
-//   return (
-//     <div>
-//       <div ref={chartRef} className="chart"></div>
-//       {selectedBubble && (
-//         <div className="popup">
-//           {/* <span className="popup-address">
-//             {selectedBubble &&
-//               selectedBubble.data &&
-//               selectedBubble.data.address}
-//           </span> */}
-//           {/* <span className="popup-address">{selectedBubble.data.address}</span> */}
-//           {console.log(selectedBubble.text)}
-
-//           <button className="popup-close" onClick={handleClosePopup}>
-//             Close
-//           </button>
-//         </div>
-//       )}
-//       {/* Rest of your code */}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// ! next try
 "use client";
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { StackDataContext } from "../utils/StackContext";
 import data from "../constant/FinalData.json";
 import * as d3 from "d3";
@@ -237,20 +17,41 @@ const Home = () => {
   const chartRef = useRef(null);
   const [selectedBubble, setSelectedBubble] = useState(null);
 
-  useEffect(() => {
-    // Find the selected data for the stack address
-    const selectedData = data.find(
-      (item) => item.stake_address === selectedAddress
-    );
+  // Truncate the text if its width exceeds maxWidth
+  function truncateText(text, maxWidth) {
+    const ellipsis = "...";
+    const textContainer = document.createElement("div");
+    textContainer.style.position = "absolute";
+    textContainer.style.visibility = "hidden";
+    textContainer.style.width = "auto";
+    textContainer.style.maxWidth = `${maxWidth}px`;
+    textContainer.textContent = text;
+    document.body.appendChild(textContainer);
 
-    // Truncate the text and add ellipsis if it exceeds maxLength
-    function truncateText(text, maxLength) {
-      if (text.length > maxLength) {
-        return text.substring(0, maxLength) + "...";
-      }
-      return text;
+    let truncatedText = text;
+    while (textContainer.scrollWidth > maxWidth && truncatedText.length > 0) {
+      truncatedText = truncatedText.slice(0, -1);
+      textContainer.textContent = truncatedText + ellipsis;
     }
 
+    const finalText = textContainer.textContent;
+    document.body.removeChild(textContainer);
+
+    return finalText;
+  }
+  const selectedData = useMemo(() => {
+    return data.find((item) => item.stake_address === selectedAddress);
+  }, [selectedAddress]);
+
+  const handleBubbleClick = useCallback((event, d) => {
+    setSelectedBubble(d.data);
+  }, []);
+
+  const handleCloseButtonClick = useCallback(() => {
+    setSelectedBubble(null);
+  }, []);
+
+  useEffect(() => {
     // Clear any previous chart
     d3.select(chartRef.current).selectAll("*").remove();
 
@@ -298,22 +99,22 @@ const Home = () => {
         .append("text")
         .attr("dy", ".3em")
         .style("text-anchor", "middle")
-        // .text((d) => d.data.address)
-        .text((d) => truncateText(d.data.address, 10)); // Specify the desired maximum length
+        .text((d) => {
+          if (d.r < 10) {
+            return "";
+          }
+          return truncateText(d.data.address, d.r * 0.7);
+        })
+        // .text((d) => truncateText(d.data.address, d.r * 0.7))
+        .style("font-size", (d) => `${Math.min(0.5 * d.r, 40)}px`);
+
+      // Inside your D3 code, where we create the text elements for the bubbles
     }
-  }, [selectedAddress]);
-
-  const handleBubbleClick = (event, d) => {
-    setSelectedBubble(d.data);
-  };
-
-  const handleCloseButtonClick = () => {
-    setSelectedBubble(null);
-  };
+  }, [handleBubbleClick, selectedAddress, selectedData]);
 
   return (
     <div>
-      <div ref={chartRef} className="chart"></div>
+      <div ref={chartRef} className="chart "></div>
       {selectedBubble && (
         <div className="bubble-details">
           <button className="close-button" onClick={handleCloseButtonClick}>
